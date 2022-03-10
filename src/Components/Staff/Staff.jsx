bef4d1a3e49e7a6d754cee2e2aa5b89f83fa97c2
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-03-10 19:16:06 +0600

    modal crud view, edit, updare, delete done

--- src/Components/Staff/Staff.jsx
+++ src/Components/Staff/Staff.jsx
@@ -4,12 +4,19 @@ import { Container, Row, Col, Button, Table, Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import StaffModal from './StaffModal/StaffModal';
 import axios from 'axios';
+import swal from 'sweetalert';
 
 
 const Staff = () => {
 
   // all staff state
   const [allStaff, setAllStaff] = useState([]);
+
+  // all modalType state
+  const [modalType, setModalType] = useState('staff_add');
+
+  // all dataId state
+  const [dataId, setDataId] = useState(0);
 
   // modal manage state
   const [modal, setModal] = useState(false);
@@ -19,12 +26,75 @@ const Staff = () => {
   const handleModalShow = () => setModal(true);
   const handleModalHide = () => setModal(false);
 
+  // staff create
+  const handleStaffCreate = (id) => {
+
+    setModalType('staff_add');
+    handleModalShow();
+
+  }
 
   // single staff view
+  const [single, setSingle] = useState('');
+  const handleViewStaff = (id) => {
+
+    setModalType('staff_view');
+    axios.get('http://localhost:8000/staff/' + id)
+    .then(res => setSingle(res.data));
+    handleModalShow();
+
+  }
 
 
+   // single staff delete
+   const handleDeleteStaff = (id) => {
+
+    swal({
+      title : 'are you sure',
+      text : 'Delete',
+      icon : 'warning',
+      buttons : true,
+      dangerMode : true
+    }).then( (data) => {
+ 
+     if(data == true){
+       
+       axios.delete('http://localhost:8000/staff/' + id).then(res => {
+ 
+         axios.get('http://localhost:8000/staff').then(res => {
+          setAllStaff(res.data.reverse());
+         });
+ 
+       });
+ 
+       swal({
+         title : 'Student Deleted Successfully',
+         icon : 'success'
+       });
+      
+ 
+ 
+     }else{
+       swal({
+         title : 'Data Safe',
+         icon : 'success'
+       });
+     }
+ 
+    });
+      
+ 
+   }
 
 
+     // single staff Edit
+  const handleEditStaff = (id) => {
+
+    setDataId(id);
+    setModalType('staff_edit');
+    handleModalShow();
+
+  }
 
 
   // data get useEffect
@@ -36,8 +106,7 @@ const Staff = () => {
   async function allStaffGet(){
 
     axios.get('http://localhost:8000/staff')
-    .then(res => setAllStaff(res.data));
-
+    .then(res => setAllStaff(res.data.reverse()));
 
   }
   
@@ -48,7 +117,7 @@ const Staff = () => {
   return (
     <>
 
-    <StaffModal show={ modal } hide={ handleModalHide }></StaffModal>
+    <StaffModal id={dataId} alutoReload={ setAllStaff } modalType={modalType} view={single} show={ modal } hide={ handleModalHide }></StaffModal>
 
      <Container>
         <Row className='mt-5'>
@@ -81,9 +150,9 @@ const Staff = () => {
                             <td>
                               <Button onClick={ () => handleViewStaff(data.id) } variant='info'> <FontAwesomeIcon icon={ faEye }></FontAwesomeIcon> </Button>&nbsp;
 
-                              <Button variant='warning'> <FontAwesomeIcon icon={ faEdit }></FontAwesomeIcon> </Button>&nbsp;
+                              <Button onClick={ () => handleEditStaff(data.id) }  variant='warning'> <FontAwesomeIcon icon={ faEdit }></FontAwesomeIcon> </Button>&nbsp;
 
-                              <Button variant='danger'> <FontAwesomeIcon icon={ faTrash }></FontAwesomeIcon> </Button>
+                              <Button onClick={ () => handleDeleteStaff(data.id) } variant='danger'> <FontAwesomeIcon icon={ faTrash }></FontAwesomeIcon> </Button>
                             </td>
                           </tr>
                         
@@ -95,7 +164,7 @@ const Staff = () => {
                     </tbody>
                 </Table>
               </Card.Body>
-              <Button onClick={ handleModalShow } variant='info'>Add New Staff</Button>
+              <Button onClick={ handleStaffCreate } variant='info'>Add New Staff</Button>
               
             </Card>
           </Col>
--- src/Components/Staff/StaffModal/StaffModal.jsx
+++ src/Components/Staff/StaffModal/StaffModal.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Button, Form, CloseButton, Modal } from 'react-bootstrap';
+import { Button, Form, CloseButton, Modal, Row, Col, Card } from 'react-bootstrap';
 import swal from 'sweetalert';
 import { useNavigate } from 'react-router-dom';
+import './StaffModal.css';
 
 
 const StaffModal = (props) => {
@@ -48,9 +49,8 @@ const StaffModal = (props) => {
       });
 
       props.hide();
-      return navigate('/staff');
-
-
+      axios.get('http://localhost:8000/staff')
+      .then(res => props.alutoReload(res.data.reverse()));
 
     }
 
@@ -59,51 +59,169 @@ const StaffModal = (props) => {
 
 
 
+  // staff edit 
+  const [edit, setEdit] = useState({
+    name    : '',
+    role    : '',
+    gender  : ''
+  });
+
+  // useEffect for staff data
+  useEffect( () => {
+    getEditStaff();
+  }, []);
+
+  // get edit data
+  async function getEditStaff(){
+    let res = await axios.get('http://localhost:8000/staff/' + props.id);
+    setEdit(res.data);
+
+  }
+
+   // input value get
+  function handleInputVal(e){
+
+    setEdit({
+      ...edit,
+      [e.target.name] : e.target.value
+    });
+
+  }
 
 
+  // staff Update
+  const handleFormUpdate = (e) => {
+    e.preventDefault();
 
-  return (
-    <>
+    axios.put('http://localhost:8000/staff/' + props.id, edit)
+    .then(res => {
+
+      props.hide();
+      swal('Staff Updated Successfully');
+     
+      axios.get('http://localhost:8000/staff')
+      .then(res => props.alutoReload(res.data.reverse()));
       
-      <Modal show={ props.show } onHide={ props.hide }>
-          <Modal.Header>
-              <h3 className="text-center"> Add New Staff</h3>
-              <CloseButton onClick={ props.hide }></CloseButton>
-          </Modal.Header>
-          <Modal.Body>
-              <Form onSubmit={ handleFormSubmit } method='POST'>
-      
-                  <Form.Group>
-                      <Form.Label>Staff</Form.Label>
-                      <Form.Control value={ staff.name } onChange={ (e) => handleInputVal(e) } name="name"></Form.Control>
-                  </Form.Group>
-  
-                  <Form.Group>
-                      <Form.Label className='d-block'>Activity</Form.Label>
-                      <Form.Select value={ staff.role } onChange={ (e) => handleInputVal(e) } name="role">
-                          <option value="">-select-</option>
-                          <option value="Manager">Manager</option>
-                          <option value="Owner">Owner</option>
-                          <option value="Director">Director</option>
-                      </Form.Select>
-                  </Form.Group>
 
-                  <Form.Label className='d-block'>Gender</Form.Label>
-                  <Form.Group value={ staff.gender } onChange={ (e) => handleInputVal(e) } className='d-flex gap-2'>
-                      <input value="male" name="gender" id='male' type="radio" /><label htmlFor="male"> Male</label>
+    });
 
-                      <input value="female" name="gender" id='female' type="radio" /><label htmlFor="female"> Female</label>
-                  </Form.Group>
-                          <br></br>
-  
-                  <Button type='submit' variant='info' className='mt-3 text-center'>Submit</Button>
-  
-              </Form>
-          </Modal.Body>
-      </Modal>
+
+  }
+
+
+
+
+  // Modal Type 
+  if(props.modalType == 'staff_add'){
+    return (
+      <>
         
-   </>
-  )
+        <Modal show={ props.show } onHide={ props.hide }>
+            <Modal.Header>
+                <h3 className="text-center"> Add New Staff</h3>
+                <CloseButton onClick={ props.hide }></CloseButton>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={ handleFormSubmit } method='POST'>
+        
+                    <Form.Group>
+                        <Form.Label>Staff</Form.Label>
+                        <Form.Control value={ staff.name } onChange={ (e) => handleInputVal(e) } name="name"></Form.Control>
+                    </Form.Group>
+    
+                    <Form.Group>
+                        <Form.Label className='d-block'>Activity</Form.Label>
+                        <Form.Select value={ staff.role } onChange={ (e) => handleInputVal(e) } name="role">
+                            <option value="">-select-</option>
+                            <option value="Manager">Manager</option>
+                            <option value="Owner">Owner</option>
+                            <option value="Director">Director</option>
+                        </Form.Select>
+                    </Form.Group>
+  
+                    <Form.Label className='d-block'>Gender</Form.Label>
+                    <Form.Group value={ staff.gender } onChange={ (e) => handleInputVal(e) } className='d-flex gap-2'>
+                        <input value="male" name="gender" id='male' type="radio" /><label htmlFor="male"> Male</label>
+  
+                        <input value="female" name="gender" id='female' type="radio" /><label htmlFor="female"> Female</label>
+                    </Form.Group>
+                            <br></br>
+    
+                    <Button type='submit' variant='info' className='mt-3 text-center'>Submit</Button>
+    
+                </Form>
+            </Modal.Body>
+        </Modal>
+          
+     </>
+    )
+  }else if(props.modalType == 'staff_view'){
+    return (
+      <>
+        
+        <Modal show={ props.show } onHide={ props.hide }>
+            <Modal.Header>
+                <h3 className="text-center"> Single Staff</h3>
+                <CloseButton onClick={ props.hide }></CloseButton>
+            </Modal.Header>
+            <Modal.Body>
+            <Row>
+                <Col md={10} className=''>
+                        <h5>Name : <span>{ props.view.name }</span></h5>    
+                        <h5>Activity : <span>{ props.view.role }</span></h5>  
+                        <h5>Gender : <span>{ props.view.gender }</span></h5>  
+                </Col>
+            </Row>
+            </Modal.Body>
+        </Modal>
+          
+     </>
+    )
+  }else if(props.modalType == 'staff_edit'){
+    return (
+      <>
+        
+        <Modal show={ props.show } onHide={ props.hide }>
+            <Modal.Header>
+                <h3 className="text-center"> Update Staff</h3>
+                <CloseButton onClick={ props.hide }></CloseButton>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={ handleFormUpdate } method='POST'>
+        
+                    <Form.Group>
+                        <Form.Label>Staff</Form.Label>
+                        <Form.Control value={ edit.name } onChange={ (e) => handleInputVal(e) } name="name"></Form.Control>
+                    </Form.Group>
+    
+                    <Form.Group>
+                        <Form.Label className='d-block'>Activity</Form.Label>
+                        <Form.Select value={ edit.role } onChange={ (e) => handleInputVal(e) } name="role">
+                            <option value="">-select-</option>
+                            <option value="Manager">Manager</option>
+                            <option value="Owner">Owner</option>
+                            <option value="Director">Director</option>
+                        </Form.Select>
+                    </Form.Group>
+  
+                    <Form.Label className='d-block'>Gender</Form.Label>
+                    <Form.Group value={ edit.gender } onChange={ (e) => handleInputVal(e) } className='d-flex gap-2'>
+                        <input value="male" name="gender" id='male' type="radio" /><label htmlFor="male"> Male</label>
+  
+                        <input value="female" name="gender" id='female' type="radio" /><label htmlFor="female"> Female</label>
+                    </Form.Group>
+                            <br></br>
+    
+                    <Button type='submit' variant='info' className='mt-3 text-center'>Submit</Button>
+    
+                </Form>
+            </Modal.Body>
+        </Modal>
+          
+     </>
+    )
+  }
+  
 };
 
 export default StaffModal;
